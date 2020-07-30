d0097843bbe3fcb71489a0038b0277c4822a2b5f
italobbarros <60980611+italobbarros@users.noreply.github.com>
2020-07-30 18:40:45 -0300

    update v1

--- Esp82666_MQTT/Esp82666_MQTT.ino
+++ Esp82666_MQTT/Esp82666_MQTT.ino
@@ -1,4 +1,21 @@
-
+// -----------------------------------------------------------------------
+//   Copyright (C) Italo Barros 2020
+// -----------------------------------------------------------------------
+//   Arquivo: Esp8266_MQTT.c
+//            Codigo para utilização de um ESP NODEMCU em automação residencial 
+//   Autor:   Italo Barbosa Barros
+//            https://www.linkedin.com/in/italobbarros/
+//   Licença: GNU GPL 2
+// -----------------------------------------------------------------------
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 2 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+// -----------------------------------------------------------------------
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
